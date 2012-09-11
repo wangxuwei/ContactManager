0a011f5ad48191957e7b137c8d3e5f2c8e0d85fc
wangxuwei <wangxuwei84@gmail.com>
2012-09-11 15:27:34 +0800

    #30 remove unused codes in app.js

--- src/main/webapp/js/4_app.js
+++ src/main/webapp/js/4_app.js
@@ -5,35 +5,5 @@ var app = app || {};
 	// if mock true use SQLiteDao,else use remote dao
 	app.mock = true;
 
-	//just keep it for now
-	//check the browsers type
-	var ua = navigator.userAgent.toLowerCase();
-	
-	var isChrome = ua.match(/chrome\/([\d.]+)/);
-	var isSafari = ua.match(/version\/([\d.]+)/);
-	var isFirefox = ua.match(/firefox\/([\d.]+)/);
-	var isIE = ua.match(/msie ([\d.]+)/);
-	
-	app.cssPrefix = function(){
-		if(isChrome){
-			return "-webkit-";
-		}else if(isSafari){
-			return "-webkit-";
-		}else if(isFirefox){
-			return "-moz-";
-		}else if(isIE){
-			return "-ms-";
-		}
-	}
-	
-	app.getTransitionEnd = function(){
-		if(isChrome){
-			return "webkitTransitionEnd";
-		}else if(isSafari){
-			return "webkitTransitionEnd";
-		}else if(isFirefox){
-			return "transitionend";
-		}
-	}
 })();
 
